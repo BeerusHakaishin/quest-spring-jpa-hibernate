002b427d80a2e92e9041ff0a10b14ec73c86e7d6
Ismaël Meziani <meziani-i@outlook.fr>
2022-01-03 22:24:09 +0100

    all done

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,10 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SchoolRepository {
+import com.wildcodeschool.wildandwizard.entity.School;
+
+// TODO : update this interface DO |X|
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
 }
